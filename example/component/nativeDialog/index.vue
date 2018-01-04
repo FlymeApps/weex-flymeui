20ae2a3ae36b7e3ab0dbd4bf580b41e9c6a5f9be
3332523marco <mapeng@mapengdeMacBook-Pro.local>
2018-01-04 15:59:27 +0800

    新增组件弹框、tab、shadow #000000

---
+++ example/component/nativeDialog/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="mzui-demo">
+    <scroller class="scroller">
+      <title link="http://apps.flyme.cn/docs/book.html?bookId=59df3445a5a0a611eee9f119&doc=59e41a2967e2271c7fc46b4e"></title>
+      <category title="展示两个按钮弹框"></category>
+        <div class="container row">
+          <fm-button class="btn" text="点击" @click="click('two_btn')" />
+        </div>
+      <category title="展示列表弹框"></category>
+        <div class="container row">
+          <fm-button class="btn" text="点击" @click="click('handle_select')" />
+        </div>
+      <category title="展示底部列表弹框"></category>
+        <div class="container row">
+          <fm-button class="btn" text="点击" @click="click('bottom_select')" />
+        </div>
+    </scroller>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+  @import "../../../packages/style/mixin.scss";
+  .mzui-demo {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background-color: #fff;
+  }
+  
+  .container {
+    padding-top: size(60);
+    padding-left: size(60);
+    padding-right: size(60);
+  }
+
+  .row {
+    flex-direction: row;
+  }
+
+  .btn {
+    margin-right: size(60);
+  }
+  
+  .scroller {
+    flex: 1;
+  }
+</style>
+
+<script>
+import { FmButton, FmText } from '../../../index';
+import Title from '../../_mods/title.vue';
+import Category from '../../_mods/category.vue';
+import { setTitle } from '../../_mods/set-nav';
+
+const modal = weex.requireModule('modal');
+const view = weex.requireModule('view')
+
+export default {
+    data() {
+            return {
+                dialogdata:''
+            }
+        },
+    components: { Title, Category, FmButton, FmText },
+    methods: {
+      click(e) {
+        if(e == 'two_btn'){
+            this.dialogdata = {
+                       title:'我的标题',
+                       positivebtn:'确定',
+                       negativebtn:'取消'
+                    };
+        }else if(e == 'handle_select'){
+            this.dialogdata = {
+                        items: [
+                            '复制',
+                            '删除'
+                        ]
+                }
+        }else if(e == 'bottom_select'){
+          this.dialogdata = {
+                            items: [
+                                '复制',
+                                '删除'
+                            ],
+                            itemcolors:[
+                                '#de3933',
+                                '#198ded'
+                            ]
+                        }
+        }
+        view.showDialog({
+                    type: e,
+                    data: this.dialogdata
+                },res => {
+                      modal.toast({message: res.data})
+            })
+      }
+    }
+}
+</script>
---
+++ example/component/nativeTab/index.vue
@@ -0,0 +1,97 @@
+<template>
+   <FmTab :tab="test" @tabPosition="ontab" class="t">
+  <!--子标签 1> -->
+  <list class="list" @loadmore="fetch" loadmoreoffset="10">
+    <cell class="cell" v-for="num in lists">
+      <div class="panel">
+        <text class="text">{{num}}</text>
+      </div>
+    </cell>
+  </list>
+ <!--子标签 2> -->
+  <list class="list" >
+    <cell class="cell" v-for="num in lists">
+      <div class="panel">
+        <text class="text">112</text>
+      </div>
+    </cell>
+  </list>
+  <!--子标签 3> -->
+ <FmLoading/>
+ <!--子标签 4> -->
+<list class="list" >
+    <cell class="cell" v-for="num in lists">
+      <div class="panel">
+        <text class="text">112</text>
+      </div>
+    </cell>
+  </list>
+    <!--子标签 5> -->
+  <list class="list" >
+    <cell class="cell" v-for="num in lists">
+      <div class="panel">
+        <text class="text">112</text>
+      </div>
+    </cell>
+  </list>
+ </FmTab>
+</template>
+
+<script>
+  const modal = weex.requireModule('modal')
+  const LOADMORE_COUNT = 4
+
+  export default {
+    data () {
+      return {
+        lists: [1, 2, 3, 4, 5],
+        test:{container:{viewpagepaddingLeft:0,viewpagepaddingRight:0,viewpagepaddingTop:80,viewpagepaddingBottom:0,tabpaddingLeft:0,tabpaddingRight:0,tabpaddingTop:0,tabpaddingBottom:0},tabs:[{text:'a222h',paddingStart:18,paddingEnd:18,minWidth:50,textunSelectColor:'#ef8394',textSelectColor:'#FF198DED'},{text:'a22sa22ssss2hsss2h',minWidth:50,paddingStart:8,paddingEnd:8,textunSelectColor:'#ef8394',textSelectColor:'#FF198DED'},{text:'ah',minWidth:30,paddingStart:5,paddingEnd:5,textunSelectColor:'#ef8394',textSelectColor:'#FF198DED'},{text:'ah',paddingStart:5,paddingEnd:5,textunSelectColor:'#ef8394',textSelectColor:'#FF198DED'},{text:'aa22ssss2hh',paddingStart:5,paddingEnd:5,textunSelectColor:'#ef8394',textSelectColor:'#FF198DED'}]}
+      }
+    },
+    methods: {
+      fetch (event) {
+        modal.toast({ message: 'loadmore', duration: 1 })
+
+        setTimeout(() => {
+          const length = this.lists.length
+          for (let i = length; i < length + LOADMORE_COUNT; ++i) {
+            this.lists.push(i + 1)
+          }
+        }, 800)
+      },
+      ontab(event){
+         modal.toast({ message: event.position, duration: 1 })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.t{
+ margin-top: 0px;
+}
+.list{
+     /*margin-bottom:180px;*/
+height:1500px;
+
+}
+  .panel {
+    width: 900px;
+    height: 250px;
+    margin-left: 75px;
+    margin-right:75px;
+    margin-top: 35px;
+    margin-bottom: 35px;
+    flex-direction: column;
+    justify-content: center;
+    border-width: 2px;
+    border-style: solid;
+    border-color: rgb(162, 217, 192);
+    background-color: rgba(162, 217, 192, 0.2);
+  }
+  .text {
+    font-size: 50px;
+    text-align: center;
+    color: #41B883;
+  }
+</style>
