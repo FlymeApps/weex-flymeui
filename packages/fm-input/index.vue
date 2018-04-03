fbe24f3a2d88123fc3c21ff54511aff21de6978c
Yanjiie <suenyinkit@163.com>
2018-04-03 09:54:33 +0800

    [ - ] remove theme and dependencies

--- packages/fm-input/index.vue
+++ packages/fm-input/index.vue
@@ -33,7 +33,61 @@
   </div>
 </template>
 
-<style lang="css" src="../../lib/theme-paint/lib/input.css" scoped>
+<style scoped>
+  .fm-wrapper {
+    padding-top: 72;
+    padding-right: 48;
+    padding-left: 48;
+  }
+
+  .fm-textarea {
+    padding-bottom: 27;
+    padding-left: 24;
+    border-bottom-style: solid;
+    border-bottom-width: 3;
+    border-bottom-color: #e6e6e6;
+    font-size: 48;
+    color: #000000;
+    placeholder-color: #dddddd;
+    caret-color: #198ded;
+  }
+
+  .fm-input-wrap {
+    position: relative;
+  }
+
+  .delete {
+    position: absolute;
+    top: 69;
+    right: 72;
+    width: 60;
+    height: 60;
+    padding: 6;
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: 30;
+  }
+
+  .visible {
+    position: absolute;
+    top: 63;
+    right: 72;
+    width: 72;
+    height: 72;
+  }
+
+  .fm-textarea:focus {
+    border-bottom-color: #198ded;
+  }
+
+  .fm-textarea-error {
+    border-bottom-color: #df2b18;
+  }
+
+  .error-msg {
+    font-size: 36;
+    color: #df2b18;
+    margin-left: 24;
+  }
 </style>
 
 <script>
